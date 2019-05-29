8eed0628dbfd43ace0b5676b9eb1869fe9e8fb9f
arinanikishenkoGITHUB <steklovaesenya@yandex.ru>
2019-05-29 20:46:54 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Drink sailor");
+        System.out.println("Bottoms Up,");
+        System.out.println("Woe on wine,");
+        System.out.println("At night staggering taverns\n");
+        System.out.println("You forget about her");
+    }
+}
